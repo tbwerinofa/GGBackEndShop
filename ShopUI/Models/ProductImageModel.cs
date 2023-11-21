cfb0b495331bdba63d538b9bbee3c73a37b0679a
tbwerinofa <tbwerinofa@gmail.com>
2023-11-21 08:16:05 +0200

    Delete ShopUI directory

--- ShopUI/Models/ProductImageModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShopUI.Models
-{
-    public class ProductImageModel
-    {
-        public int Id { get; set; }
-        public int ProductId { get; set; }
-        public IFormFile FormFile { get; set; }
-    }
-}
--- ShopUI/Models/SaveResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShopUI.Models
-{
-    public class SaveResult
-    {
-        public int Id { get; set; }
-        public bool IsSuccess { get; set; }
-        public string Message { get; set; }
-    }
-}
